1ea94cf1685c31f09331316a5ddb8e4bcfc0813c
daejinKim <kdjalberta@gmail.com>
2025-10-12 00:38:09 -0600

    dollarsign problem

--- Csharp-OLTP-shopping/DMIT2018Project/obj/Debug/net8.0/DMIT2018Project.AssemblyInfo.cs
+++ Csharp-OLTP-shopping/DMIT2018Project/obj/Debug/net8.0/DMIT2018Project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMIT2018Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3aa2d1072ede033bc8c42a301711c1b53c1c7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43044f6a4752f48e437e8ce91e8d50ad43549d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMIT2018Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMIT2018Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Csharp-OLTP-shopping/RentalSystem/obj/Debug/net8.0/RentalSystem.AssemblyInfo.cs
+++ Csharp-OLTP-shopping/RentalSystem/obj/Debug/net8.0/RentalSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3aa2d1072ede033bc8c42a301711c1b53c1c7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43044f6a4752f48e437e8ce91e8d50ad43549d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
