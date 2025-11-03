05809de8cd87b7c7dc15126305fd740ff399b348
daejinKim <kdjalberta@gmail.com>
2025-11-02 18:21:22 -0700

    react native practice starts

---
+++ ReactNative/app/about.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+//rnfe
+const about = () => {
+  return (
+    <View>
+      <Text>about</Text>
+    </View>
+  )
+}
+
+export default about
---
+++ ReactNative/app/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Content testing</Text>
+      <Link href="/about">Visit About Screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //min-height: 100vh 같은느낌인듯.. 세로공간 100%차지
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
