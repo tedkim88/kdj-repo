9ca235b24dd8b4ed7b8461a2f21fd0ccbd1d30b2
daejinKim <kdjalberta@gmail.com>
2025-11-24 16:16:03 -0700

    some weird..files

--- Csharp-OLTP-shopping/RentalSystem/obj/Debug/net8.0/RentalSystem.AssemblyInfo.cs
+++ Csharp-OLTP-shopping/RentalSystem/obj/Debug/net8.0/RentalSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c1a0bbfe5e6815bf899979ce00eef6034e61fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f746cedf95981dd4f06676aaa744bbd647c81929")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Csharp-OLTP-shopping/SalesSystem/obj/Debug/net8.0/SalesSystem.AssemblyInfo.cs
+++ Csharp-OLTP-shopping/SalesSystem/obj/Debug/net8.0/SalesSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c1a0bbfe5e6815bf899979ce00eef6034e61fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f746cedf95981dd4f06676aaa744bbd647c81929")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
